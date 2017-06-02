2cbe023d6c0c78e3db1c2329c1ce7e5aeb7a1308
lelema-srv <lelema.zh@gmail.com>
2017-06-02 17:34:45 -0400

    reimplement PrintFullCallingContext

--- panda/plugins/trace_deadwrite/trace_deadwrite.cpp
+++ panda/plugins/trace_deadwrite/trace_deadwrite.cpp
@@ -2569,46 +2569,54 @@ bool init_plugin(void *self) {
     // Given a context node (curContext), traverses up in the chain till the root and prints the entire calling context 
     
     VOID PrintFullCallingContext(ContextNode * curContext){
-        int depth = 0;
 #ifdef MULTI_THREADED        
         int root;
 #endif         //end MULTI_THREADED
         // set sig handler
-        struct sigaction old;
-        sigaction(SIGSEGV,&gSigAct,&old);
+        //struct sigaction old;
+        //sigaction(SIGSEGV,&gSigAct,&old);
         
+    CallStack callstack = {0};
+    callstack.n = get_callers(callstack.callers, MAX_CCT_PRINT_DEPTH, env);
+    callstack.pc = p.pc;
+    callstack.asid = p.cr3;
+
+        int depth = callstack.n;
         // Dont print if the depth is more than MAX_CCT_PRINT_DEPTH since files become too large
-        while(curContext && (depth ++ < MAX_CCT_PRINT_DEPTH)){            
+        while(curContext && (depth -- >= 0)){            
             if(IsValidIP(curContext->address)){
-                if(PIN_UndecorateSymbolName(RTN_FindNameByAddress(curContext->address),UNDECORATION_COMPLETE) == ".plt"){
-                    if(setjmp(env) == 0) {
+                //lele: TODO
+                 fprintf(gTraceFile, "\n! " TARGET_FMT_lx, callstack.callers[depth]);
+                 // Also get the full stack here
+                // if(PIN_UndecorateSymbolName(RTN_FindNameByAddress(curContext->address),UNDECORATION_COMPLETE) == ".plt"){
+                //     if(setjmp(env) == 0) {
                         
-                        if(IsValidPLTSignature(curContext) ) { 
-                            uint64_t nextByte = (uint64_t) curContext->address + 2;
-                            int * offset = (int*) nextByte;
+                //         if(IsValidPLTSignature(curContext) ) { 
+                //             uint64_t nextByte = (uint64_t) curContext->address + 2;
+                //             int * offset = (int*) nextByte;
                             
-                            uint64_t nextInst = (uint64_t) curContext->address + 6;
-                            ADDRINT loc = *((uint64_t *)(nextInst + *offset));
-                            if(IsValidIP(loc)){
-                                fprintf(gTraceFile,"\n!%s",PIN_UndecorateSymbolName(RTN_FindNameByAddress(loc),UNDECORATION_COMPLETE).c_str() );
-                            }else{
-                                fprintf(gTraceFile,"\nIN PLT BUT NOT VALID GOT");	
-                            } 
-                        } else {
-                            fprintf(gTraceFile,"\nUNRECOGNIZED PLT SIGNATURE");	
+                //             uint64_t nextInst = (uint64_t) curContext->address + 6;
+                //             ADDRINT loc = *((uint64_t *)(nextInst + *offset));
+                //             if(IsValidIP(loc)){
+                //                 fprintf(gTraceFile,"\n!%s",PIN_UndecorateSymbolName(RTN_FindNameByAddress(loc),UNDECORATION_COMPLETE).c_str() );
+                //             }else{
+                //                 fprintf(gTraceFile,"\nIN PLT BUT NOT VALID GOT");	
+                //             } 
+                //         } else {
+                //             fprintf(gTraceFile,"\nUNRECOGNIZED PLT SIGNATURE");	
                             
-                            //fprintf(gTraceFile,"\n plt plt plt %x", * ((UINT32*)curContext->address));	
-                            //for(int i = 1; i < 4 ; i++)
-                            //	fprintf(gTraceFile," %x",  ((UINT32 *)curContext->address)[i]);	
+                //             //fprintf(gTraceFile,"\n plt plt plt %x", * ((UINT32*)curContext->address));	
+                //             //for(int i = 1; i < 4 ; i++)
+                //             //	fprintf(gTraceFile," %x",  ((UINT32 *)curContext->address)[i]);	
                             
-                        }
-                    }   
-                    else {
-                        fprintf(gTraceFile,"\nCRASHED !!");	
-                    }
-                } else {
-                    fprintf(gTraceFile,"\n%s",PIN_UndecorateSymbolName(RTN_FindNameByAddress(curContext->address),UNDECORATION_COMPLETE).c_str() );
-                }
+                //         }
+                //     }   
+                //     else {
+                //         fprintf(gTraceFile,"\nCRASHED !!");	
+                //     }
+                // } else {
+                //     fprintf(gTraceFile,"\n%s",PIN_UndecorateSymbolName(RTN_FindNameByAddress(curContext->address),UNDECORATION_COMPLETE).c_str() );
+                // }
             } 
 #ifndef MULTI_THREADED 
             else if (curContext == gRootContext){
